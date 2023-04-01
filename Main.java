1d23786395640b44b69a24f16baab1f1d546a12c
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-04-01 12:30:30 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+    int number1 = 12;
+    int number2 = 15;
+
+    // is equal to
+    System.out.println(number1 == number2);
+
+    // is not equal to
+    System.out.println(number1 != number2);
+
+    // is greater to
+    System.out.println(number1 > number2);
+
+    // is less to
+    System.out.println(number1 < number2);
+
+    // is greater than or equal to
+    System.out.println(number1 >= number2);
+
+    // is less than or equal to
+    System.out.println(number1 <= number2);
+  }
+}
